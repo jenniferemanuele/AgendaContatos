49f1263d52791b086afec2fbe1c2bc586ba62fe5
jenniferemanuele <jenniferemanuele950@gmail.com>
2023-06-30 19:55:16 -0300

    Agenda_0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import  { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+function ListaContatos() {
+  const [contatos, setContatos] = useState([]);
+  const [nome, setNome] = useState('');
+  const [numero, setNumero] = useState('');
+
+  const adicionarContato = () => {
+    if (nome !== '' && numero !== '') {
+      const novoContato = { nome, numero };
+      setContatos([...contatos, novoContato]);
+      setNome('');
+      setNumero('');
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="lista-contatos">
+      <h2>Lista de Contatos</h2>
+      <ul>
+        {contatos.map((contato, index) => (
+          <li key={index}>
+            Nome: {contato.nome}, Número: {contato.numero}
+          </li>
+        ))}
+      </ul>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Nome"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Número"
+          value={numero}
+          onChange={(e) => setNumero(e.target.value)}
+        />
+        <button onClick={adicionarContato}>Adicionar</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default ListaContatos;
